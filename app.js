852d48d312f57020b445efae616faa970eae312b
FransJoakim <fransjlt@gmail.com>
2022-03-28 13:34:33 +0200

    cors-setings changed to fix problem

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
-var cors = require('cors');
-app.use(cors());
-
 app.use(express.json());
+var cors = require('cors');
+app.use(cors({
+  origin: "https://myprivatechef.herokuapp.com/",
+  credentials: true
+}));
+
 
 app.get('/', (req, res) => {
   res.json('hello world!');
 });
 
-module.exports.app = app; 
+module.exports.app = app;
