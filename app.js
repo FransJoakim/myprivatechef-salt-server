c90eba3df9a3b17da326f75254c93c907dcf66a4
SumanaSattar <sumana.sattar@gmail.com>
2022-03-28 17:49:24 +0200

    db  fixed

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 var cors = require('cors');
-
+const { createNewCart }  =  require('./chefDatabase/db')
 app.use(express.json());
 app.use(cors());
+
+createNewCart()
 
 app.get('/', (req, res) => {
   res.json('hello barbara!');
---
+++ chefDatabase/db.js
@@ -0,0 +1,31 @@
+const { MongoClient } = require('mongodb');
+
+require('dotenv').config()
+const url = `mongodb+srv://masterchef:${process.env.DB_PASSWORD}@cluster0.ubqlg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const dbName = 'myprivatechef';
+console.log(process.env.DB_PASSWORD)
+const mongoConnection = client => {
+    const db = client.db(dbName);
+    db.on('close', () => { console.log('close connection'); });
+    db.on('reconnect', () => { console.log('reconnected'); });
+    const chefCollection = db.collection('chefs');
+    return chefCollection;
+  };
+  
+  const createNewCart = async () => {
+    const client = await MongoClient
+      .connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+  
+    try {
+      const chefCollection = mongoConnection(client);
+      const result = await chefCollection.find();
+      console.log(result)
+      return result
+    } catch (err) {
+      throw new Error(err);
+    } finally {
+      client.close();
+    }
+  };
+  
+  module.exports.createNewCart = createNewCart
