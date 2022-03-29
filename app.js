293864ee092355ccad2d3bcc0133e387b30fba8e
Barbara Kogus <barbarakogus@gmail.com>
2022-03-29 21:51:02 +0200

    add healthcheck endpoint

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
+app.get('/', async (req, res) => {
+  res.send('server is up');
+  res.end();
+});
+
 app.get('/chefs', async (req, res) => {
   const result = await getAllChefs();
   console.log(result);
